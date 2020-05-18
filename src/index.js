b26bffd9f276dab2f5b9615f41ee8588878a65f7
Binh 🌱 <binhrobles@gmail.com>
2020-05-18 12:53:32 -0700

    Revert "migrate to typescript because i like pain" because why would I do that
    This reverts commit c70ab6ce70b496cbb58011eb4a6d78aee9ca1afa.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,12 @@ import './index.css';
 import Game from './game';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Game />, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <Game />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
