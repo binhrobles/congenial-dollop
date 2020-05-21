20cbeb929381a4631ff270d50d5c88f708ddd237
Binh 🌱 <binhrobles@gmail.com>
2020-05-21 12:41:22 -0700

    Update PORT mapping

--- src/keys.js
+++ src/keys.js
@@ -1,7 +1,7 @@
 const keys = Object.freeze({
   debug: process.env.REACT_APP_DEBUG || false,
-  port: process.env.REACT_APP_PORT || 8000,
-  serverUri: process.env.REACT_APP_SERVER_URI || `http://localhost:8000`,
+  serverUri:
+    process.env.REACT_APP_SERVER_URI || `http://${window.location.hostname}`,
 });
 
 export default keys;
--- src/server.js
+++ src/server.js
@@ -2,15 +2,15 @@ const Server = require('boardgame.io/server').Server;
 const path = require('path');
 const serve = require('koa-static');
 const Game = require('./game').default;
-const keys = require('./keys').default;
 
 const server = Server({ games: [Game] });
+const port = process.env.PORT || 8000;
 
 // Build path relative to the server.js file
 const frontEndAppBuildPath = path.resolve(__dirname, '../build');
 server.app.use(serve(frontEndAppBuildPath));
 
-server.run(keys.port, () => {
+server.run(port, () => {
   server.app.use(async (ctx, next) =>
     serve(frontEndAppBuildPath)(
       Object.assign(ctx, { path: 'index.html' }),
