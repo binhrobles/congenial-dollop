008979a90160895817a4cd8496869b1ca9013161
Binh 🌱 <binhrobles@gmail.com>
2020-05-21 12:52:25 -0700

    Hit the https route on heroku

--- src/keys.js
+++ src/keys.js
@@ -1,7 +1,7 @@
 const keys = Object.freeze({
   debug: process.env.REACT_APP_DEBUG || false,
   serverUri:
-    process.env.REACT_APP_SERVER_URI || `http://${window.location.hostname}`,
+    process.env.REACT_APP_SERVER_URI || `https://${window.location.hostname}`,
 });
 
 export default keys;
