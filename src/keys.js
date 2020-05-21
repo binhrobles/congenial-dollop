5eb203d3d3a5a372658ad939329f3bfdfa9e4ad4
Binh 🌱 <binhrobles@gmail.com>
2020-05-21 12:32:48 -0700

    Serve client w/ server and integrate Github Action CI step

--- src/keys.js
+++ src/keys.js
@@ -1,6 +1,7 @@
 const keys = Object.freeze({
   debug: process.env.REACT_APP_DEBUG || false,
-  serverUri: process.env.REACT_APP_SERVER_URI || 'http://localhost:8000',
+  port: process.env.REACT_APP_PORT || 8000,
+  serverUri: process.env.REACT_APP_SERVER_URI || `http://localhost:8000`,
 });
 
 export default keys;
--- src/server.js
+++ src/server.js
@@ -1,5 +1,20 @@
 const Server = require('boardgame.io/server').Server;
+const path = require('path');
+const serve = require('koa-static');
 const Game = require('./game').default;
+const keys = require('./keys').default;
 
 const server = Server({ games: [Game] });
-server.run(8000);
+
+// Build path relative to the server.js file
+const frontEndAppBuildPath = path.resolve(__dirname, '../build');
+server.app.use(serve(frontEndAppBuildPath));
+
+server.run(keys.port, () => {
+  server.app.use(async (ctx, next) =>
+    serve(frontEndAppBuildPath)(
+      Object.assign(ctx, { path: 'index.html' }),
+      next
+    )
+  );
+});
