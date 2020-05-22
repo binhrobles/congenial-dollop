53a3d48ba6760c45e198216ea8a4553baab3b061
Binh 🌱 <binhrobles@gmail.com>
2020-05-22 00:28:19 -0700

    client: remove trailing /

--- src/keys.js
+++ src/keys.js
@@ -1,8 +1,7 @@
 const keys = Object.freeze({
   debug: process.env.REACT_APP_DEBUG || false,
   serverUri:
-    process.env.REACT_APP_SERVER_URI ||
-    `https://shitty-thirteen.herokuapp.com/`,
+    process.env.REACT_APP_SERVER_URI || `https://shitty-thirteen.herokuapp.com`,
 });
 
 export default keys;
