25235192d52b3699429f07a8edebd172f5ce3ea0
Binh 🌱 <binhrobles@gmail.com>
2020-05-22 00:21:24 -0700

    server: stop serving react app

--- src/server.js
+++ src/server.js
@@ -1,20 +1,7 @@
 const Server = require('boardgame.io/server').Server;
-const path = require('path');
-const serve = require('koa-static');
 const Game = require('./game').default;
 
 const server = Server({ games: [Game] });
 const port = process.env.PORT || 8000;
 
-// Build path relative to the server.js file
-const frontEndAppBuildPath = path.resolve(__dirname, '../build');
-server.app.use(serve(frontEndAppBuildPath));
-
-server.run(port, () => {
-  server.app.use(async (ctx, next) =>
-    serve(frontEndAppBuildPath)(
-      Object.assign(ctx, { path: 'index.html' }),
-      next
-    )
-  );
-});
+server.run(port);
