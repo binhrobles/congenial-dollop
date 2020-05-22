09431aa2cb580043ca3a06d0f375cff33065b619
Binh 🌱 <binhrobles@gmail.com>
2020-05-21 23:11:17 -0700

    client: point at heroku backend

--- src/keys.js
+++ src/keys.js
@@ -1,7 +1,8 @@
 const keys = Object.freeze({
   debug: process.env.REACT_APP_DEBUG || false,
   serverUri:
-    process.env.REACT_APP_SERVER_URI || `https://${window.location.hostname}`,
+    process.env.REACT_APP_SERVER_URI ||
+    `https://shitty-thirteen.herokuapp.com/`,
 });
 
 export default keys;
