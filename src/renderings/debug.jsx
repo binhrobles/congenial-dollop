e41af4cd0d8667504020c314bc4ede6dbc324278
Binh 🌱 <binhrobles@gmail.com>
2020-05-26 01:29:05 -0700

    Add a sick fancy background

--- src/renderings/debug.jsx
+++ src/renderings/debug.jsx
@@ -11,7 +11,7 @@ function Debug(props) {
   const { G, ctx, playerID, isActive, moves } = props;
 
   return (
-    <>
+    <div style={{ backgroundColor: '#35654d' }}>
       <Row
         align="center"
         style={{
@@ -29,7 +29,7 @@ function Debug(props) {
           moves={moves}
         />
       )}
-    </>
+    </div>
   );
 }
 
